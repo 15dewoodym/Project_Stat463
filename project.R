4f9ea87ab5e4b523dc7e65a8bed08cbba00f86a6
Melissa DeWoody <mad6051@psu.edu>
2018-11-07 11:15:25 -0500

    Small Change made to forecast

--- project.R
+++ project.R
@@ -31,7 +31,7 @@ modM = estimate(SARIMA(ar = 2, i = 0, ma = 1, sar = 2, si = 0, sma = 2, s = 7), 
 check(modM)
 predict(modM, n.ahead = 30)
 
-mobile_pred <- predict(modM, n.ahead = 1, level = 0.95)
+mobile_forecast<- predict(modM, n.ahead = 1, level = 0.95)
 mobile_object = list(mobile_pred = as.numeric(mobile_forecast$pred), 
                      mobile_ci = as.numeric(mobile_forecast$CI0.95))
 
@@ -43,7 +43,7 @@ modD = estimate(SARIMA(ar = 1, i = 0, ma = 1, sar = 2, si = 0, sma = 1, s = 7), 
 check(modD)
 predict(modD, n.ahead = 30)
 
-desktop_pred <- predict(modD, n.ahead = 1, level = 0.95)
+desktop_forecast <- predict(modD, n.ahead = 1, level = 0.95)
 desktop_object = list(desktop_pred = as.numeric(desktop_forecast$pred), 
                      desktop_ci = as.numeric(desktop_forecast$CI0.95))
 
@@ -57,7 +57,7 @@ check(modS)
 predict(modS, n.ahead = 30)
 
 
-silvio_pred = predict(modS, n.ahead = 1, level = 0.95)
+silvio_forecast = predict(modS, n.ahead = 1, level = 0.95)
 silvio_object = list(silvio_pred = as.numeric(silvio_forecast$pred), 
                      silvio_ci = as.numeric(silvio_forecast$CI0.95))
 
@@ -71,7 +71,7 @@ modB = estimate(AR(9),St)
 check(modB)
 predict(modB, n.ahead = 30)
 
-beyonce_pred = predict(modB, n.ahead = 1, level = 0.95)
+beyonce_forecast = predict(modB, n.ahead = 1, level = 0.95)
 beyonce_object = list(beyonce_pred = as.numeric(beyonce_forecast$pred), 
                       beyonce_ci = as.numeric(beyonce_forecast$CI0.95))
 
@@ -84,7 +84,7 @@ modC = estimate(AR(6),Ct)
 check(modC)
 predict(modC, n.ahead = 10)
 
-chomsky_pred = predict(modC, n.ahead = 1, level = 0.95)
+chomsky_forecast = predict(modC, n.ahead = 1, level = 0.95)
 chomsky_object = list(chomsky_pred = as.numeric(chomsky_forecast$pred), 
                       chomsky_ci = as.numeric(chomsky_forecast$CI0.95))
 
@@ -97,7 +97,7 @@ modL = estimate(AR(9), Lt)
 check(modL)
 predict(modL, n.ahead = 30)
 
-lazio_pred = predict(modL, n.ahead = 1, level = 0.95)
+lazio_forecast= predict(modL, n.ahead = 1, level = 0.95)
 lazio_object = list(lazio_pred = as.numeric(lazio_forecast$pred), 
                     lazio_ci = as.numeric(lazio_forecast$CI0.95))
 
@@ -112,7 +112,7 @@ modT = estimate(SARIMA(ar = 2, i = 0, ma = 1, sar = 1, si = 0, sma = 1, s = 42),
 check(modT)
 predict(modT, n.ahead = 30)
 
-thanks_pred = predict(modT, n.ahead = 1, level = 0.95)
+thanks_forecast= predict(modT, n.ahead = 1, level = 0.95)
 thanks_object = list(thanks_pred = as.numeric(thanks_forecast$pred), 
                      thanks_ci = as.numeric(thanks_forecast$CI0.95))
 
